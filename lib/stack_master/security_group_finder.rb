e2d0b66a30cd41176cc9f4266054328917620af4
lethalpaga <lethalpaga@gmail.com>
2015-12-22 15:16:46 +1300

    Fallback to name tag to find security groups

--- lib/stack_master/security_group_finder.rb
+++ lib/stack_master/security_group_finder.rb
@@ -10,19 +10,42 @@ module StackMaster
     def find(reference)
       raise ArgumentError, 'Security group references must be non-empty strings' unless reference.is_a?(String) && !reference.empty?
 
-      groups = @resource.security_groups({
-                                           filters: [
-                                             {
-                                               name: "group-name",
-                                               values: [reference],
-                                             },
-                                           ],
-                                         })
+      # Try with a group-name first
+      groups = @resource.security_groups(group_name_filter(reference))
+
+      # If not found, try with a Name tag
+      groups = @resource.security_groups(tag_name_filter(reference)) unless groups.any?
 
       raise SecurityGroupNotFound, "No security group with name #{reference} found" unless groups.any?
       raise MultipleSecurityGroupsFound, "More than one security group with name #{reference} found" if groups.count > 1
 
       groups.first.id
+    end
+
+    def group_name_filter(reference)
+      {
+        filters: [
+          {
+            name: 'group-name',
+            values: [reference],
+          },
+        ],
+      }
+    end
+
+    def tag_name_filter(reference)
+      {
+        filters: [
+          {
+            name: 'tag-key',
+            values: ['Name'],
+          },
+          {
+            name: 'tag-value',
+            values: [reference],
+          },
+        ],
+      }
     end
   end
 end
